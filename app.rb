ff8cdd8ce4450677255dbe64ffc1050cfbe721b4
Alex Bonwit <alexbonwit@mac.com>
2019-12-09 17:50:38 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Alex"
+    end
+
+    get '/hometown' do
+        "My hometown is Valley View, Pennsylvania."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Bone Machine by the Pixies."
+    end
+
 end
